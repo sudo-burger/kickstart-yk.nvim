82c65cd6e7a33c6ffa47b34844eba22b60557413
yk <federico@pietrolucci.net>
2024-03-16 02:49:13 +0100

    wiki: enable completion for existing wiki pages.

--- lua/custom/plugins/wiki-vim.lua
+++ lua/custom/plugins/wiki-vim.lua
@@ -1,9 +1,20 @@
 return {
   {
     'lervag/wiki.vim',
+    -- Enable completion for wiki pages.
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/cmp-omni',
+    },
     init = function()
       vim.g.wiki_root = '~/devs/vimwiki'
       vim.g.wiki_completion_case_sensitive = 0
+      local cmp = require 'cmp'
+      cmp.setup.filetype('markdown', {
+        sources = cmp.config.sources {
+          { name = 'omni' },
+        },
+      })
     end,
   },
 }
