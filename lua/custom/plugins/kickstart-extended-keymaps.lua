181adbbc245732f26ebcfa4ce87170f0320849db
yk <federico@pietrolucci.net>
2024-03-16 01:07:35 +0100

    extended-keymaps: registerd "toggle" group.

--- lua/custom/plugins/kickstart-extended-keymaps.lua
+++ lua/custom/plugins/kickstart-extended-keymaps.lua
@@ -2,6 +2,9 @@ return {
   {
     -- Additional keymaps for nvim-cmp.
     --
+    require('which-key').register {
+      ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+    },
     require('cmp').setup {
       mapping = require('cmp').mapping.preset.insert {
         ['<C-b>'] = require('cmp').mapping.scroll_docs(-1),
