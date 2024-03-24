acfca6215ab98e1c0b512ed14534d4a21d40d93f
yk <federico@pietrolucci.net>
2024-03-24 23:33:32 +0100

    DAP: cleanup.

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -1,13 +1,37 @@
 return {
-  'rcarriga/nvim-dap-ui',
+  'mfussenegger/nvim-dap',
   dependencies = {
-    'mfussenegger/nvim-dap',
-    'mfussenegger/nvim-dap-python',
+    'rcarriga/nvim-dap-ui',
+    -- Required dependency for nvim-dap-ui
+    'nvim-neotest/nvim-nio',
+    --
+    -- Installs the debug adapters for you
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+
     'nvim-telescope/telescope-dap.nvim',
     'theHamsta/nvim-dap-virtual-text',
+
+    'mfussenegger/nvim-dap-python',
   },
   config = function()
     local dap, dapui = require 'dap', require 'dapui'
+
+    require('mason-nvim-dap').setup {
+      -- Makes a best effort to setup the various debuggers with
+      -- reasonable debug configurations
+      automatic_setup = true,
+
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      -- handlers = {},
+
+      -- You'll need to check that you have the required things installed
+      -- online, please don't ask me how to install them :)
+      -- Update this to ensure that you have the debuggers for the langs you want
+      -- ensure_installed = {},
+    }
+
     dapui.setup()
     --
     -- Show variable values inline, as "virtual text".
@@ -15,15 +39,10 @@ return {
 
     -- Help functions.
     -- Open/close cleanly.
-    dap.listeners.after.event_initialized['dapui_config'] = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated['dapui_config'] = function()
-      dapui.close()
-    end
-    dap.listeners.before.event_exited['dapui_config'] = function()
-      dapui.close()
-    end
+    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+    dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
     require('which-key').register {
       ['<leader>cd'] = { name = '[d]ebugger', _ = 'which_key_ignore' },
     }
@@ -34,5 +53,7 @@ return {
     vim.keymap.set('n', '<leader>cdr', dap.repl.open, { desc = '[r]epl' })
     vim.keymap.set('n', '<leader>cds', dap.step_into, { desc = '[s]tep into' })
     vim.keymap.set('n', '<leader>cdS', dap.step_over, { desc = '[S]tep over' })
+    --
+    require('dap-python').setup()
   end,
 }
--- lua/custom/plugins/python.lua
+++ lua/custom/plugins/python.lua
@@ -1,9 +1,2 @@
 vim.g.python3_host_prog = '~/devs/python/venv/nvim/bin/python'
-return {
-  'mfussenegger/nvim-dap-python',
-  dependencies = {
-    'mfussenegger/nvim-dap',
-  },
-  -- Language-specific.
-  require('dap-python').setup(),
-}
+return {}
